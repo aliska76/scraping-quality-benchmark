ee51e6e77b33b282db4d59f089967b0389020339
Alisa <alisara@post.jce.ac.il>
2026-04-14 05:16:10 +0300

    Delete scraper directory

--- scraper/__init__.py
+++
@@ -1,6 +0,0 @@
-from scraper.base import BaseFetcher
-from scraper.fetcher import HTTPFetcher
-from scraper.browser import PlaywrightFetcher
-from scraper.selenium_browser import SeleniumFetcher
-
-__all__ = ['BaseFetcher', 'HTTPFetcher', 'PlaywrightFetcher', 'SeleniumFetcher']
